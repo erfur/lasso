8f487df162ee3ddd3a8316370ca47aeafbcf4b61
erfur <erfur@mailbox.org>
2024-01-28 20:06:06 +0100

    move application view

--- app/src/main/java/com/github/erfur/lasso/MainActivity.kt
+++ app/src/main/java/com/github/erfur/lasso/MainActivity.kt
@@ -1,16 +1,8 @@
 package com.github.erfur.lasso
 
-import android.content.Intent
-import android.content.pm.ApplicationInfo
-import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.github.erfur.lasso.databinding.ActivityMainBinding
-import com.topjohnwu.superuser.ipc.RootService
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
